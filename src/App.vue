e970d65c0548eb4326d85c430178c543e2af52ae
Stucks95 <simo.stuck@gmail.com>
2024-05-04 00:08:51 +0200

    theme fix

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@ import Toolbar from './components/Toolbar.vue'
 import { RouterLink, RouterView } from 'vue-router'
 
 const pages = [
-  { path: '/', name: 'home', showedName: 'Home' },
-  { path: '/about', name: 'about', showedName: 'About' },
-  { path: '/gallery', name: 'gallery', showedName: 'Gallery' },
-  { path: '/contact', name: 'contact', showedName: 'Contact' },
+  { path: '/', name: 'home', showedName: 'Home', classIcon: 'pi pi-home' },
+  { path: '/about', name: 'about', showedName: 'About', classIcon: 'pi pi-id-card' },
+  { path: '/gallery', name: 'gallery', showedName: 'Gallery', classIcon: 'pi pi-image' },
+  { path: '/contact', name: 'contact', showedName: 'Contact', classIcon: 'pi pi-user' },
 ]
 </script>
 
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -3,46 +3,56 @@
         <template #start>
             <div class="flex align-items-center gap-2">
                 <div v-for="page in pages" :key="page">
-                    <RouterLink class="hover:shadow-6" :to="{ name: page.name }">
-                        {{ page.showedName }}
+                    <RouterLink class="hover:shadow-6 flex" :to="{ name: page.name }">
+                        <i :class="page.classIcon" class="mr-1"></i>{{ page.showedName }}
                     </RouterLink>
                 </div>
-                <RouterLink class="hover:shadow-6" :to="{ name: 'booking' }">
-                    Book your stay
+                <RouterLink class="hover:shadow-6 flex" :to="{ name: 'booking' }">
+                    <i class="pi pi-bookmark mr-1"></i>Book your stay 
                 </RouterLink>
             </div>
         </template>
 
         <template #end>
             <div class="flex align-items-center gap-2">
-                <i v-if="themeToggle" class="pi pi-moon" style="color: slateblue"></i>
-                <i v-if="!themeToggle" class="pi pi-sun" style="color: slateblue"></i>
-                <InputSwitch v-model="themeToggle" @change="changeTheme" />
+                <i v-if="themeCheck" class="pi pi-moon" style="color: slateblue"></i>
+                <i v-if="!themeCheck" class="pi pi-sun" style="color: slateblue"></i>
+                <InputSwitch id="theme-switch" v-model="themeCheck" @change="toggleTheme" />
             </div>
         </template>
     </Toolbar>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue'
 import { usePrimeVue } from 'primevue/config';
+import { useDark, useToggle } from '@vueuse/core'
 
+const primeVue = usePrimeVue()
+const lightTheme = 'aura-light-blue'
+const darkTheme = 'aura-dark-blue'
+const linkId = 'id-to-link'
+let nextTheme = ''
+let currentTheme = lightTheme
+let themeCheck = ref(false)
+
+const toggleTheme = () => {
+    if (currentTheme === lightTheme) nextTheme = darkTheme;
+    else if (currentTheme === darkTheme) nextTheme = lightTheme;
+    // CHANGE THEME METHOD DOESN'T WORK ON BUILD APP !!
+    //primeVue.changeTheme(currentTheme, nextTheme, linkId, () => {});  
+    currentTheme = nextTheme
+    let elToRemove = document.getElementById(linkId)
+    let elToAdd = elToRemove.cloneNode()
+    elToAdd.setAttribute("href", "/public/themes/"+currentTheme+"/theme.css")
+    document.getElementById('index-head').appendChild(elToAdd)
+    elToRemove.remove()
+}
+</script>
+
+<script>
 export default {
-    props: ['pages'],
-    setup() {
-        const PrimeVue = usePrimeVue();
-        let themeToggle = ref(false)
-
-        const changeTheme = () => {
-            if (themeToggle.value) {
-                PrimeVue.changeTheme('aura-light-blue', 'aura-dark-blue', 'theme-link', () => {})
-            } else {
-                PrimeVue.changeTheme('aura-dark-blue', 'aura-light-blue', 'theme-link', () => {})
-            }
-        }
-
-        return { themeToggle, changeTheme }
-    }
+    props: ['pages']
 }
 </script>
 
